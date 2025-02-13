475e0e78e4057f5f535353106c2e957e6c9b7da7
Yash <yashvardhansingh232@gmail.com>
2025-02-14 01:02:47 +0530

    made profile component

---
+++ profile-component/src/App.jsx
@@ -0,0 +1,28 @@
+
+function App() {
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className="w-[400px] h-[500px] border-2 flex flex-col items-center justify-around">
+        <div>
+          bg-photo
+        </div>
+          
+        <div>
+          profile-photo
+        </div>
+
+        <div>
+          description
+        </div>
+
+        <div>
+          features
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
